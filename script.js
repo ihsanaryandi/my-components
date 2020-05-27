07c780025a275da781bf323707c0a674adfe5644
ihsanaryandi <jfrosty426@gmail.com>
2020-05-27 08:56:16 +0700

    Adding Icon component and Dropdown Component

--- script.js
+++ script.js
@@ -42,3 +42,34 @@ function autoformat(inputElement, itemsElement) {
 	})
 }
 
+// Icon with ripple effect
+const icons = document.querySelectorAll('.icon')
+
+icons.forEach(icon => {
+	icon.addEventListener('click', () => {
+		const ripple = document.createElement('div')
+		icon.appendChild(ripple)
+		setTimeout(() => ripple.remove(), 500)
+	})
+})
+
+
+// Dropdown
+const dropdowns = document.querySelectorAll('.dropdown')
+
+document.addEventListener('click', e => {
+	dropdowns.forEach(dropdown => {
+		if(dropdown.children[0] !== e.target) {
+			dropdown.children[0].classList.remove('open')
+			dropdown.children[1].classList.remove('open')
+		}
+	})
+
+	if(e.target.parentElement.classList.contains('dropdown')) {
+		const dropdown = e.target
+
+		dropdown.classList.toggle('open')
+		return dropdown.nextElementSibling.classList.toggle('open')
+	}
+})
+
